cd2f4036001eb21be2fe82d9360ed3c664325b5a
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-07 08:47:56 +0300

    detect a face on the video

---
+++ detect_face_in_video.py
@@ -0,0 +1,25 @@
+import cv2
+
+video=cv2.VideoCapture('video/video.mp4')
+success,frame=video.read()
+
+#loading xml file for recognising faces
+face_cascade=cv2.CascadeClassifier('faces.xml')
+# param for video a new video from an existing one
+height = frame.shape[0]
+width = frame.shape[1]
+# create an empty file/video
+output=cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'DIVX'),30,(width,height))
+while success:
+    # find diff sizes of the face
+    faces=face_cascade.detectMultiScale(frame,1.1,4) # based on the frames
+    #place this cordinates on the img
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),4)
+    output.write(frame)
+    success,frame=video.read() # get the next frame/img
+
+# generate video and store it on MAC
+output.release()
+
+
